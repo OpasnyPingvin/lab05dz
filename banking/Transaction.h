2266347c8809a749cd2b3695e84541ccc0c275af
Pingvin <128987484+OpasnyPingvin@users.noreply.github.com>
2026-04-15 14:32:52 +0300

    Update Transaction.h

--- banking/Transaction.h
+++ banking/Transaction.h
@@ -11,15 +11,10 @@ class Transaction {
   int fee() const { return fee_; }
   void set_fee(int fee) { fee_ = fee; }
 
- private:
   void Credit(Account& accout, int sum);
   bool Debit(Account& accout, int sum);
-
-  // Virtual to test.
   virtual void SaveToDataBase(Account& from, Account& to, int sum);
 
+ private:
   int fee_;
-
-  friend class MockAccount;
-  friend class TestTransaction;
 };
